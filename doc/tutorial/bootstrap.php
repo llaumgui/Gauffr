257b1f5c3d3dcd21fe21e2cf28de8ee38ea75638
llaumgui <llaumgui@fd4d8e13-4472-41bd-a50c-e3e8cea423e9>
2010-08-13 15:19:23 +0000

    oups

--- doc/tutorial/bootstrap.php
+++ doc/tutorial/bootstrap.php
@@ -6,8 +6,7 @@ if ( !defined('GAUFFR__ENABLED') )
      * Check your include path.
      * If eZ Components or Gauffr is not in include path, add it:
      */
-    #set_include_path(get_include_path() . PATH_SEPARATOR . "/my/ezc/path/" . PATH_SEPARATOR . "/my/gauffr/path/");
-    set_include_path(get_include_path() . PATH_SEPARATOR . "D:\wamp\www\ezpublish-4.3\lib\ezc" . PATH_SEPARATOR . "../../");
+    set_include_path(get_include_path() . PATH_SEPARATOR . "/my/ezc/path/" . PATH_SEPARATOR . "/my/gauffr/path/");
 
     include ('Gauffr/gauffr.php' );
 }
