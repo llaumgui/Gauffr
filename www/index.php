65a5a15b835ddadacc2424bcf3cbcfcc6bd7afc2
llaumgui <llaumgui@fd4d8e13-4472-41bd-a50c-e3e8cea423e9>
2010-08-13 22:52:08 +0000

    InitGauffrAdmin

---
+++ www/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * GauffrAdmin index.php.
+ *
+ * @version //autogentag//
+ * @package GauffrAdmin
+ * @copyright Copyright (c) 2010 Guillaume Kulakowski and contributors
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0
+ */
+
+require 'Gauffr/gauffr.php';
+
+/*
+ * Packager : put GauffrAdmin in php_path (/urs/share/php) and chage the line bellow.
+ */
+require '../GauffrAdmin/config.php';
+
+// Instantiate the dispatcher configuration object.
+//$config = new helloMvcConfiguration();
+
+// Send the configuration to the dispatcher, and run it.
+//$dispatcher = new ezcMvcConfigurableDispatcher( $config );
+//$dispatcher->run();
+
+?>
