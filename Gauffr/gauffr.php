4e3390dc81b0dbde697c1c82d2867ed98da93b27
llaumgui <llaumgui@fd4d8e13-4472-41bd-a50c-e3e8cea423e9>
2010-08-25 17:03:16 +0000

    v0.4

--- Gauffr/gauffr.php
+++ Gauffr/gauffr.php
@@ -27,7 +27,7 @@ class Gauffr
     /**
      * Application version.
      */
-    const APP_VERSION = "0.4-dev";
+    const APP_VERSION = "0.4";
     /**
      * Gauffr global configuration file.
      */
--- build-tools/build.sh
+++ build-tools/build.sh
@@ -10,39 +10,61 @@ API_PATH=doc/api
 GAUFFR_VERSION=0.4
 GAUFFR_PATH=../
 BUILD_PATH=../../gauffr-${GAUFFR_VERSION}
+ECHO="echo -e"
+
+# motd
+${ECHO} "\n#\n# Gauffr builder\n# build version ${GAUFFR_VERSION}\n#\n"
 
 
 # Pre cleanup
+${ECHO} "# Clean build path - Start"
 if [ -d ${BUILD_PATH} ]; then
 	rm -rf ${BUILD_PATH}
 fi
+${ECHO} "# Clean build path - End\n"
 
 
 # Create package
+${ECHO} "# Create package - Start"
 mkdir ${BUILD_PATH}
 cp -R ${GAUFFR_PATH}bin ${BUILD_PATH}
 cp -R ${GAUFFR_PATH}doc ${BUILD_PATH}
 cp -R ${GAUFFR_PATH}build-tools/Doxyfile ${BUILD_PATH}/doc
 cp -R ${GAUFFR_PATH}Gauffr ${BUILD_PATH}
+${ECHO} "# Create package - End\n"
 
 
 # Use gauffr.ini
+${ECHO} "# Use gauffr.ini - Start"
 rm -f  ${BUILD_PATH}/Gauffr/conf/gauffr.ini
 mv ${BUILD_PATH}/Gauffr/conf/gauffr.ini.exemple \
      ${BUILD_PATH}/Gauffr/conf/gauffr.ini
+${ECHO} "# Use gauffr.ini - End\n"
+
 
 # Cleanup .svn
-find ${BUILD_PATH}/ -name '.svn' -type d -exec rm -rf {} \;
+${ECHO} "# Cleanup .svn - Start"
+find ${BUILD_PATH}/ -name '.svn' -type d -exec rm -rf {} \; >/dev/null 2>&1
+${ECHO} "# Cleanup .svn - End\n"
+
 
 # Tag version in header
+${ECHO} "# Tag version in header - Start"
 find ${BUILD_PATH}/Gauffr/ -name '*.php' -exec sed -i "s/@version \/\/autogentag\/\//@version ${GAUFFR_VERSION}/g" {} \;
-sed -i "s/@version \/\/autogentag\/\//@version ${GAUFFR_VERSION}/g" ${BUILD_PATH}/Doxyfile;
+sed -i "s/\/\/autogentag\/\//${GAUFFR_VERSION}/g" ${BUILD_PATH}/doc/Doxyfile;
+${ECHO} "# Tag version in header - End\n"
 
-# phpDoc
+
+# Doxygen
+${ECHO} "# Doxygen - Start"
 if [ -d ${BUILD_PATH}}/${API_PATH} ]; then
     rm -rf ${BUILD_PATH}}/${API_PATH}
 fi
-doxygen ${BUILD_PATH}/doc/Doxyfile
+cd ${BUILD_PATH}/doc ; doxygen Doxyfile
+${ECHO} "# Doxygen - End\n"
+
 
 # tar.gz
-tar -czvf ${BUILD_PATH}.tar.gz ${BUILD_PATH}/
+${ECHO} "# tar - Start"
+tar -czf ${BUILD_PATH}.tar.gz ${BUILD_PATH}/
+${ECHO} "# tar - End\n\n"
