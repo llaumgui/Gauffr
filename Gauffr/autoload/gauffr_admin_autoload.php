973c37c88372673610122e70904436ac478c2bb7
llaumgui <llaumgui@fd4d8e13-4472-41bd-a50c-e3e8cea423e9>
2011-02-18 18:17:56 +0000

    update copyright

--- Gauffr/autoload/gauffr_admin_autoload.php
+++ Gauffr/autoload/gauffr_admin_autoload.php
@@ -4,7 +4,7 @@
  *
  * @version //autogentag//
  * @package GauffrAdmin
- * @copyright Copyright (c) 2009-2010 Guillaume Kulakowski and contributors
+ * @copyright Copyright (c) 2009-2011 Guillaume Kulakowski and contributors
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0
  * @ignore
  */
--- bin/gauffr_clear_log.php
+++ bin/gauffr_clear_log.php
@@ -8,7 +8,7 @@
  *
  * @version //autogentag//
  * @package Gauffr
- * @copyright Copyright (c) 2009-2010 Guillaume Kulakowski and contributors
+ * @copyright Copyright (c) 2009-2011 Guillaume Kulakowski and contributors
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0
  */
 
