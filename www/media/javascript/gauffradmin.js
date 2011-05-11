9202f4fe7c3146c86def1edc65c8914a61a36cdf
llaumgui <llaumgui@fd4d8e13-4472-41bd-a50c-e3e8cea423e9>
2011-05-11 14:41:46 +0000

    Update jQuery, add jQuery.URL, use min version and add a ThirdParty doc with all third party script

--- www/media/javascript/gauffradmin.js
+++ www/media/javascript/gauffradmin.js
@@ -28,43 +28,19 @@ $(document).ready
  * Go to on change
  */
 $.fn.gauffrGoOnChange = function () {
-	var urlVars = getUrlVars();
+	var urlParams = $.url.paramAll();
 
 	return this.each( function() {
 		$(this).change( function() {
 			// Put or override get variable
-			urlVars[this.name] = this.value;
-			urlVars['offset'] = 0; // Reset offset
+			urlParams[this.name] = this.value;
+			urlParams['offset'] = 0; // Reset offset
+			urlParamsString = '?';
 
-			urlVarsString = '?';
-			$.each( urlVars, function( name, value) {
-				urlVarsString = urlVarsString + name + "=" + value + "&";
+			$.each( urlParams, function( name, value) {
+				urlParamsString = urlParamsString + name + "=" + value + "&";
 			});
-			window.location = window.location.pathname + urlVarsString;
+			window.location = window.location.pathname + urlParamsString;
 		});
 	});
 };
-
-
-
-
-
-/* _________________________________________________________ jQuery functions */
-/**
- * Read a page's GET URL variables and return them as an associative array.
- */
-function getUrlVars() {
-    var vars = {}, hash;
-    if ( window.location.href.indexOf('?') > 0 )
-    {
-    	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    	for(var i = 0; i < hashes.length; i++) {
-    		if( hashes[i].indexOf('=') > 0 )
-			{
-    			hash = hashes[i].split('=');
-    			vars[hash[0]] = hash[1];
-			}
-    	}
-    }
-    return vars;
-}
